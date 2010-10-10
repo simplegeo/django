c015000ce8796986ce1e67c0542f5106b829b7dd
lukeplant <lukeplant@bcc190cf-cafb-0310-a4f2-bffc1f526a37>
2010-10-10 01:06:42 +0000

    Converted contrib/auth/tokens doctests to unittests.  We've always said "no more" to doctests.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@14100 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/auth/tests/__init__.py
+++ django/contrib/auth/tests/__init__.py
@@ -5,7 +5,7 @@ from django.contrib.auth.tests.forms import UserCreationFormTest, Authentication
 from django.contrib.auth.tests.remote_user \
         import RemoteUserTest, RemoteUserNoCreateTest, RemoteUserCustomTest
 from django.contrib.auth.tests.models import ProfileTestCase
-from django.contrib.auth.tests.tokens import TOKEN_GENERATOR_TESTS
+from django.contrib.auth.tests.tokens import TokenGeneratorTest
 from django.contrib.auth.tests.views \
         import PasswordResetTest, ChangePasswordTest, LoginTest, LogoutTest
 
@@ -13,5 +13,4 @@ from django.contrib.auth.tests.views \
 
 __test__ = {
     'BASIC_TESTS': BASIC_TESTS,
-    'TOKEN_GENERATOR_TESTS': TOKEN_GENERATOR_TESTS,
 }
